f4362da35398b0f06c9e2dbcd789311588ca559b
whades <101385345+whadesb@users.noreply.github.com>
2024-06-05 09:55:01 +0200

    Update property.js

--- routes/property.js
+++ routes/property.js
@@ -1,28 +1,30 @@
-// property.js
-
 const express = require('express');
 const router = express.Router();
-const Property = require('../models/Property'); // Assurez-vous de définir ce modèle
+const Property = require('../models/Property');
 const fs = require('fs');
 const path = require('path');
 
-// Décommente cette route si tu veux utiliser la route /add-property dans property.js
-/*
 router.post('/add-property', async (req, res) => {
   const { rooms, surface, price, city, country } = req.body;
   const property = new Property({ rooms, surface, price, city, country });
-  await property.save();
 
-  // Générer la landing page
-  const landingPageUrl = await generateLandingPage(property);
+  try {
+    await property.save();
 
-  property.url = landingPageUrl;
-  await property.save();
+    // Générer la landing page
+    const landingPageUrl = await generateLandingPage(property);
 
-  // Envoyer une réponse avec un message de confirmation et l'URL de la page générée
-  res.status(200).send(`Le bien immobilier a été ajouté avec succès. Vous pouvez le voir ici : ${landingPageUrl}`);
+    property.url = landingPageUrl;
+    await property.save();
+
+    // Envoyer une réponse avec un message de confirmation et l'URL de la page générée
+    res.status(200).send(`Le bien immobilier a été ajouté avec succès. Vous pouvez le voir ici : ${landingPageUrl}`);
+  } catch (error) {
+    console.error('Erreur lors de l\'ajout de la propriété : ', error);
+    // En cas d'erreur, renvoyer une réponse JSON avec le statut 500 et un message d'erreur approprié
+    res.status(500).json({ error: 'Une erreur est survenue lors de l\'ajout de la propriété.' });
+  }
 });
-*/
 
 async function generateLandingPage(property) {
   const template = `
