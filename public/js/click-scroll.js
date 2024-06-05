8cfc1e31d74825a7a93eed8407180aa034be6662
whades <101385345+whadesb@users.noreply.github.com>
2024-06-05 14:55:08 +0200

    Update click-scroll.js

--- public/js/click-scroll.js
+++ public/js/click-scroll.js
@@ -4,34 +4,36 @@
 var sectionArray = [1, 2, 3, 4, 5];
 
 $.each(sectionArray, function(index, value){
-          
-     $(document).scroll(function(){
-         var offsetSection = $('#' + 'section_' + value).offset().top - 75;
-         var docScroll = $(document).scrollTop();
-         var docScroll1 = docScroll + 1;
-         
-        
-         if ( docScroll1 >= offsetSection ){
-             $('.navbar-nav .nav-item .nav-link').removeClass('active');
-             $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');  
-             $('.navbar-nav .nav-item .nav-link').eq(index).addClass('active');
-             $('.navbar-nav .nav-item .nav-link').eq(index).removeClass('inactive');
-         }
-         
-     });
-    
-    $('.click-scroll').eq(index).click(function(e){
-        var offsetClick = $('#' + 'section_' + value).offset().top - 75;
-        e.preventDefault();
-        $('html, body').animate({
-            'scrollTop':offsetClick
-        }, 300)
+    $(document).scroll(function(){
+        var section = $('#' + 'section_' + value);
+        if (section.length) { // Vérifiez si l'élément cible existe
+            var offsetSection = section.offset().top - 75;
+            var docScroll = $(document).scrollTop();
+            var docScroll1 = docScroll + 1;
+            
+            if (docScroll1 >= offsetSection) {
+                $('.navbar-nav .nav-item .nav-link').removeClass('active');
+                $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');
+                $('.navbar-nav .nav-item .nav-link').eq(index).addClass('active');
+                $('.navbar-nav .nav-item .nav-link').eq(index).removeClass('inactive');
+            }
+        }
     });
-    
+
+    $('.click-scroll').eq(index).click(function(e){
+        var section = $('#' + 'section_' + value);
+        if (section.length) { // Vérifiez si l'élément cible existe
+            var offsetClick = section.offset().top - 75;
+            e.preventDefault();
+            $('html, body').animate({
+                'scrollTop': offsetClick
+            }, 300);
+        }
+    });
 });
 
 $(document).ready(function(){
-    $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');    
+    $('.navbar-nav .nav-item .nav-link:link').addClass('inactive');
     $('.navbar-nav .nav-item .nav-link').eq(0).addClass('active');
     $('.navbar-nav .nav-item .nav-link:link').eq(0).removeClass('inactive');
 });
