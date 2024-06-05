a6d904e9118212a2fff45b53da169cb67592fd4d
whades <101385345+whadesb@users.noreply.github.com>
2024-06-05 10:09:24 +0200

    Update add-property.js

--- routes/add-property.js
+++ routes/add-property.js
@@ -41,45 +41,46 @@ async function generateLandingPage(property) {
   <head>
       <link rel="stylesheet" href="/css/bootstrap.min.css">
       <style>
-          /* Styles CSS personnalisés */
-          body {
-              font-family: Arial, sans-serif;
-              background-color: #f8f9fa;
-              color: #333;
-              margin: 0;
-              padding: 0;
-          }
-          .container {
-              max-width: 800px;
-              margin: auto;
-              padding: 20px;
-          }
-          .property-info {
-              background-color: #fff;
-              border-radius: 10px;
-              padding: 20px;
-              box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-          }
-          .property-info h1 {
-              font-size: 32px;
-              margin-bottom: 20px;
-          }
-          .property-info p {
-              font-size: 18px;
-              margin-bottom: 10px;
-          }
-          @media (max-width: 768px) {
-              .container {
-                  padding: 10px;
-              }
-              .property-info h1 {
-                  font-size: 28px;
-              }
-              .property-info p {
-                  font-size: 16px;
-              }
-          }
-      </style>
+        /* Styles CSS personnalisés */
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f8f9fa;
+            color: #333;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center; /* Centrer horizontalement */
+            align-items: center; /* Centrer verticalement */
+            height: 100vh; /* 100% de la hauteur de l'écran */
+        }
+        .container {
+            max-width: 800px;
+            padding: 20px;
+            text-align: center; /* Centrer le contenu */
+            background-color: #fff; /* Couleur de fond du contenu */
+            border-radius: 10px;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+        }
+        .property-info h1 {
+            font-size: 32px;
+            margin-bottom: 20px;
+        }
+        .property-info p {
+            font-size: 18px;
+            margin-bottom: 10px;
+        }
+        @media (max-width: 768px) {
+            .container {
+                padding: 10px;
+            }
+            .property-info h1 {
+                font-size: 28px;
+            }
+            .property-info p {
+                font-size: 16px;
+            }
+        }
+    </style>
   </head>
   <body>
       <div class="container">
