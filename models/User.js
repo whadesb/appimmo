246ba7b44e4193d9598f0cba414eece64b12f11d
whades <101385345+whadesb@users.noreply.github.com>
2024-06-06 15:48:36 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const { v4: uuidv4 } = require('uuid');
 
 const userSchema = new Schema({
+  userId: { type: String, default: uuidv4 }, // Ajouter un champ userId
   email: { type: String, required: true, unique: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
