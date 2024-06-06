1206bd1e4b39e4e334b1ac168b9b828f62c600ac
whades <101385345+whadesb@users.noreply.github.com>
2024-06-06 22:55:55 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,15 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const propertySchema = new Schema({
-  rooms: { type: Number, required: true },
-  surface: { type: Number, required: true },
-  price: { type: Number, required: true },
-  city: { type: String, required: true },
-  country: { type: String, required: true },
-  createdBy: { type: Schema.Types.ObjectId, ref: 'User', required: true } // Référence vers le modèle User
+const userSchema = new Schema({
+  email: { type: String, required: true, unique: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  role: { type: String, required: true },
+  password: { type: String, required: true }
 });
 
-const Property = mongoose.model('Property', propertySchema);
-
-module.exports = Property;
+module.exports = mongoose.model('User', userSchema);
