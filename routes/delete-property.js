fcbe1acf647f5dd38b9e94253d56e589cd7774c1
whades <101385345+whadesb@users.noreply.github.com>
2024-06-06 21:58:53 +0200

    Update delete-property.js

--- routes/delete-property.js
+++ routes/delete-property.js
@@ -2,19 +2,20 @@
 const express = require('express');
 const router = express.Router();
 const Property = require('../models/Property');
+const authMiddleware = require('../middleware/auth'); // Importer le middleware d'authentification
 
-router.post('/delete-property/:id', async (req, res) => {
+router.delete('/delete-property/:id', authMiddleware, async (req, res) => {
+  const propertyId = req.params.id;
+
   try {
-    if (!req.session.user) {
-      return res.redirect('/login');
+    const property = await Property.findOneAndDelete({ _id: propertyId, owner: req.session.user._id });
+    if (!property) {
+      return res.status(404).json({ error: 'Propriété non trouvée ou vous n\'êtes pas autorisé à la supprimer.' });
     }
-
-    await Property.findOneAndDelete({ _id: req.params.id, user: req.session.user._id });
-
-    res.redirect('/user/properties');
+    res.status(200).json({ message: 'Propriété supprimée avec succès.' });
   } catch (error) {
     console.error('Erreur lors de la suppression de la propriété : ', error);
-    res.send('Une erreur est survenue lors de la suppression de la propriété.');
+    res.status(500).json({ error: 'Une erreur est survenue lors de la suppression de la propriété.' });
   }
 });
 
