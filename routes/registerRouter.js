d1501aeb698e96582c25359579134e105ec9d1a1
whades <101385345+whadesb@users.noreply.github.com>
2024-06-06 12:45:03 +0200

    Update registerRouter.js

--- routes/registerRouter.js
+++ routes/registerRouter.js
@@ -2,6 +2,16 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
+const nodemailer = require('nodemailer');
+
+// Configurer le transporteur pour l'envoi d'e-mails
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'communication@zebrito.fr',
+    pass: '528721Tt**'
+  }
+});
 
 // Route pour afficher le formulaire d'inscription
 router.get('/register', (req, res) => {
@@ -38,6 +48,14 @@ router.post('/register', async (req, res) => {
 
     // Sauvegarder l'utilisateur dans la base de données
     await user.save();
+
+    // Envoi d'un e-mail de confirmation
+    await transporter.sendMail({
+      from: 'votre_adresse_email@gmail.com',
+      to: email,
+      subject: 'Confirmation de votre inscription',
+      html: `<p>Bonjour ${firstName},</p><p>Votre inscription sur notre site a été confirmée avec succès.</p><p>Cordialement,</p><p>Votre équipe UAP Immo</p>`
+    });
 
     // Rediriger vers la page de connexion après l'inscription
     res.redirect('/login');
