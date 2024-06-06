68425159a01f7ffdfd1e7cfd1122ffe65eb9ce67
whades <101385345+whadesb@users.noreply.github.com>
2024-06-06 13:25:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -109,7 +109,43 @@ app.get('/register', (req, res) => {
   res.redirect('/register');
 });
 
+app.post('/register', async (req, res) => {
+  const { email, firstName, lastName, role, password, confirmPassword } = req.body;
 
+  // Vérification des mots de passe
+  if (password !== confirmPassword) {
+    return res.send('Les mots de passe ne correspondent pas.');
+  }
+
+  try {
+    // si l'utilisateur existe déjà
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.send('Un utilisateur avec cet email existe déjà.');
+    }
+
+    // Hacher le mot de passe
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Créer un nouvel utilisateur
+    const user = new User({
+      email,
+      firstName,
+      lastName,
+      role,
+      password: hashedPassword
+    });
+
+    // Sauvegarder l'utilisateur dans la base de données
+    await user.save();
+
+    // Rediriger vers la page de connexion après l'inscription
+    res.redirect('/login');
+  } catch (error) {
+    console.error('Error registering user', error);
+    res.send('Une erreur est survenue lors de l\'inscription.');
+  }
+});
 
     // Hacher le mot de passe
     const hashedPassword = await bcrypt.hash(password, 10);
