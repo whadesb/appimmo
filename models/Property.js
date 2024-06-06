0297c362e33b31fafdcc1e5e6e1dbd0f19981015
whades <101385345+whadesb@users.noreply.github.com>
2024-06-06 22:40:38 +0200

    Update Property.js

--- models/Property.js
+++ models/Property.js
@@ -1,14 +1,16 @@
-// models/Property.js
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const propertySchema = new mongoose.Schema({
+const propertySchema = new Schema({
   rooms: Number,
   surface: Number,
   price: Number,
   city: String,
   country: String,
+  views: { type: Number, default: 0 },
+  createdAt: { type: Date, default: Date.now },
   url: String,
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' } // Référence à l'utilisateur
+  createdBy: { type: Schema.Types.ObjectId, ref: 'User' } // Ajout du champ createdBy
 });
 
 module.exports = mongoose.model('Property', propertySchema);
