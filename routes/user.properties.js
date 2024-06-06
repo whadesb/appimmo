76624e4f1f1ad8985ad14ca2727a42884da8e95c
whades <101385345+whadesb@users.noreply.github.com>
2024-06-06 21:58:32 +0200

    Update user.properties.js

--- routes/user.properties.js
+++ routes/user.properties.js
@@ -2,19 +2,15 @@
 const express = require('express');
 const router = express.Router();
 const Property = require('../models/Property');
+const authMiddleware = require('../middleware/auth'); // Importer le middleware d'authentification
 
-router.get('/user/properties', async (req, res) => {
+router.get('/user-properties', authMiddleware, async (req, res) => {
   try {
-    if (!req.session.user) {
-      return res.redirect('/login');
-    }
-
-    const properties = await Property.find({ user: req.session.user._id });
-
-    res.render('user-properties', { user: req.session.user, properties });
+    const properties = await Property.find({ owner: req.session.user._id });
+    res.status(200).json(properties);
   } catch (error) {
-    console.error('Erreur lors de la récupération des propriétés de l\'utilisateur : ', error);
-    res.send('Une erreur est survenue lors de la récupération des propriétés.');
+    console.error('Erreur lors de la récupération des propriétés : ', error);
+    res.status(500).json({ error: 'Une erreur est survenue lors de la récupération des propriétés.' });
   }
 });
 
