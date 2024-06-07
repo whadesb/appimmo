2faec0c4cc51e1c764191e366473f306449eb494
whades <101385345+whadesb@users.noreply.github.com>
2024-06-07 07:47:52 +0200

    Delete middleware directory

--- middleware/auth.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res, next) => {
-    if (!req.session.user) {
-      return res.status(401).send('Vous devez être connecté pour accéder à cette ressource.');
-    }
-    next();
-  };
-  
