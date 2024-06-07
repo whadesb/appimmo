9b9e3028ac2db2a141e341fd0d23188362d54572
whades <101385345+whadesb@users.noreply.github.com>
2024-06-07 07:49:32 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -9,4 +9,6 @@ const userSchema = new Schema({
   password: { type: String, required: true }
 });
 
-module.exports = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
