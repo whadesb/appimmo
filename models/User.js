9f03d2d0157f08937161a03051340ffee2611881
whades <101385345+whadesb@users.noreply.github.com>
2024-06-08 10:32:43 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
 
-const userSchema = new Schema({
+const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   role: { type: String, required: true },
-  password: { type: String, required: true }
 });
 
-const User = mongoose.model('User', userSchema);
+userSchema.plugin(passportLocalMongoose);
 
-module.exports = User;
+module.exports = mongoose.model('User', userSchema);
