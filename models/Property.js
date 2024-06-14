46bc8e9d7822beb574d0dc46e4a90167563c1893
whades <101385345+whadesb@users.noreply.github.com>
2024-06-14 08:46:58 +0200

    Update Property.js

--- models/Property.js
+++ models/Property.js
@@ -2,42 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const propertySchema = new Schema({
-    rooms: {
-        type: Number,
-        required: true
-    },
-    surface: {
-        type: Number,
-        required: true
-    },
-    price: {
-        type: Number,
-        required: true
-    },
-    city: {
-        type: String,
-        required: true
-    },
-    country: {
-        type: String,
-        required: true
-    },
-    url: {
-        type: String,
-        required: false 
-    },
-    createdAt: { 
-        type: Date, 
-        default: Date.now 
-    },
-    views: { 
-        type: Number, 
-        default: 0 
-    },
-    user: { 
-        type: Schema.Types.ObjectId, 
-        ref: 'User' 
-    }
+    rooms: { type: Number, required: true },
+    surface: { type: Number, required: true },
+    price: { type: Number, required: true },
+    city: { type: String, required: true },
+    country: { type: String, required: true },
+    url: { type: String, required: false },
+    createdAt: { type: Date, default: Date.now },
+    views: { type: Number, default: 0 },
+    createdBy: { type: Schema.Types.ObjectId, ref: 'User' },
+    photos: [String] // Ajoutez ce champ pour stocker les chemins des photos
 });
 
 const Property = mongoose.model('Property', propertySchema);
