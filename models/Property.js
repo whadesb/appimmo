c88e4d356c013aa6227711e70c802d8e1b5f0f7e
whades <101385345+whadesb@users.noreply.github.com>
2024-08-07 17:36:21 +0200

    Update Property.js

--- models/Property.js
+++ models/Property.js
@@ -1,15 +1,19 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const propertySchema = new mongoose.Schema({
-  rooms: Number,
-  surface: Number,
-  price: Number,
-  city: String,
-  country: String,
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Référence à l'utilisateur
-  photos: [String],
-  status: { type: String, default: 'draft' }, // Ajoutez un statut par défaut
-  url: String
+const propertySchema = new Schema({
+    rooms: { type: Number, required: true },
+    surface: { type: Number, required: true },
+    price: { type: Number, required: true },
+    city: { type: String, required: true },
+    country: { type: String, required: true },
+    url: { type: String, required: false },
+    createdAt: { type: Date, default: Date.now },
+    views: { type: Number, default: 0 },
+    createdBy: { type: Schema.Types.ObjectId, ref: 'User' },
+    photos: [String] // Ajoutez ce champ pour stocker les chemins des photos
 });
 
-module.exports = mongoose.model('Property', propertySchema);
+const Property = mongoose.model('Property', propertySchema);
+
+module.exports = Property;
