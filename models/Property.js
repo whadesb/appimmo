42e8d8ff9e5c89f2879b268043b3c723037d78ad
whades <101385345+whadesb@users.noreply.github.com>
2024-08-07 17:07:52 +0200

    Update Property.js

--- models/Property.js
+++ models/Property.js
@@ -1,20 +1,15 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const propertySchema = new Schema({
-  rooms: { type: Number, required: true },
-  surface: { type: Number, required: true },
-  price: { type: Number, required: true },
-  city: { type: String, required: true },
-  country: { type: String, required: true },
-  url: { type: String, required: false },
-  createdAt: { type: Date, default: Date.now },
-  views: { type: Number, default: 0 },
-  createdBy: { type: Schema.Types.ObjectId, ref: 'User' }, // Reference to User
-  photos: [String], // Store paths of photos
-  status: { type: String, enum: ['draft', 'validated'], default: 'draft' }, // Status field
+const propertySchema = new mongoose.Schema({
+  rooms: Number,
+  surface: Number,
+  price: Number,
+  city: String,
+  country: String,
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  photos: [String],
+  status: String,
+  url: String
 });
 
-const Property = mongoose.model('Property', propertySchema);
-
-module.exports = Property;
+module.exports = mongoose.model('Property', propertySchema);
