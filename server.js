98f2c6baf76e60765acfb6e40d9107b048cb8f86
whades <101385345+whadesb@users.noreply.github.com>
2024-08-07 10:48:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -120,16 +120,18 @@ function isAuthenticated(req, res, next) {
 }
 app.get('/user', isAuthenticated, async (req, res) => {
   try {
-    // Récupérer les propriétés associées à l'utilisateur connecté
     const properties = await Property.find({ createdBy: req.user._id });
-
-    // Rendre la vue 'user' en passant les propriétés récupérées
     res.render('user', { user: req.user, properties });
   } catch (error) {
     console.error('Erreur lors de la récupération des propriétés :', error);
     res.status(500).send('Erreur lors de la récupération des propriétés.');
   }
 });
+
+app.post('/property/update/:id', isAuthenticated, async (req, res) => {
+  // Logic for updating a property
+});
+
 app.get('/faq', (req, res) => {
   res.render('faq', { title: 'faq' });
 });
