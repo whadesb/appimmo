69df570f3265ae1b2704c2a8f347ad16bcd629bd
whades <101385345+whadesb@users.noreply.github.com>
2024-08-07 16:33:03 +0200

    Update Property.js

--- models/Property.js
+++ models/Property.js
@@ -2,17 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const propertySchema = new Schema({
-    rooms: { type: Number, required: true },
-    surface: { type: Number, required: true },
-    price: { type: Number, required: true },
-    city: { type: String, required: true },
-    country: { type: String, required: true },
-    url: { type: String, required: false },
-    createdAt: { type: Date, default: Date.now },
-    views: { type: Number, default: 0 },
-    createdBy: { type: Schema.Types.ObjectId, ref: 'User' },
-    photos: [String], // Ajoutez ce champ pour stocker les chemins des photos
-    status: { type: String, enum: ['draft', 'validated'], default: 'draft' } // Nouveau champ pour le statut
+  rooms: { type: Number, required: true },
+  surface: { type: Number, required: true },
+  price: { type: Number, required: true },
+  city: { type: String, required: true },
+  country: { type: String, required: true },
+  url: { type: String, required: false },
+  createdAt: { type: Date, default: Date.now },
+  views: { type: Number, default: 0 },
+  createdBy: { type: Schema.Types.ObjectId, ref: 'User' }, // Reference to User
+  photos: [String], // Store paths of photos
+  status: { type: String, enum: ['draft', 'validated'], default: 'draft' }, // Status field
 });
 
 const Property = mongoose.model('Property', propertySchema);
