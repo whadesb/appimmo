c24edddb00ca28463469ddc3f0cc3057c6d87cea
whades <101385345+whadesb@users.noreply.github.com>
2024-08-19 09:59:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -48,6 +48,7 @@ app.use(session({
   store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }),
   cookie: { maxAge: 1000 * 60 * 5 } // Cookie de 5 minutes
 }));
+
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy({
