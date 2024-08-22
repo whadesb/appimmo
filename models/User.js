54a85f160bd9381746daf7418cb6847024a83a90
whades <101385345+whadesb@users.noreply.github.com>
2024-08-22 15:19:56 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true }, // Ajout du champ username
   email: { type: String, unique: true, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   role: { type: String, required: true },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date
 });
 
-// Application de passport-local-mongoose pour gérer l'authentification
 UserSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
 
 module.exports = mongoose.model('User', UserSchema);
