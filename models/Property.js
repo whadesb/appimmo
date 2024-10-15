6b9c0c5ecf4285b96e97d6fbe7fa731e3b9c67a3
whades <101385345+whadesb@users.noreply.github.com>
2024-10-15 08:41:58 +0200

    Update Property.js

--- models/Property.js
+++ models/Property.js
@@ -8,9 +8,22 @@ const propertySchema = new Schema({
     price: { type: Number, required: true },
     city: { type: String, required: true },
     country: { type: String, required: true },
-    yearBuilt: { type: Number, required: false }, // Année de construction
-    pool: { type: Boolean, default: false }, // Piscine
-    propertyType: { type: String, required: true }, // Type de bien
+    yearBuilt: { type: Number, required: false },
+    pool: { type: Boolean, default: false },
+    propertyType: { type: String, required: true },
+    bathrooms: { type: Number, required: false }, // Salles de douche
+    toilets: { type: Number, required: false }, // Toilettes
+    elevator: { type: Boolean, default: false }, // Ascenseur
+    fireplace: { type: Boolean, default: false }, // Cheminée
+    internet: { type: Boolean, default: false }, // Internet
+    doubleGlazing: { type: Boolean, default: false }, // Double vitrage
+    wateringSystem: { type: Boolean, default: false }, // Arrosage
+    barbecue: { type: Boolean, default: false }, // Barbecue
+    carShelter: { type: Boolean, default: false }, // Abri de voiture
+    parking: { type: Boolean, default: false }, // Parking
+    caretakerHouse: { type: Boolean, default: false }, // Maison de gardien
+    electricShutters: { type: Boolean, default: false }, // Stores électriques
+    outdoorLighting: { type: Boolean, default: false }, // Éclairage extérieur
     url: { type: String, required: false },
     createdAt: { type: Date, default: Date.now },
     views: { type: Number, default: 0 },
