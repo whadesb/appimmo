ee7add564659a25d71d6e417c89c59c7d29fb537
whades <101385345+whadesb@users.noreply.github.com>
2024-10-15 08:48:58 +0200

    Update Property.js

--- models/Property.js
+++ models/Property.js
@@ -8,22 +8,23 @@ const propertySchema = new Schema({
     price: { type: Number, required: true },
     city: { type: String, required: true },
     country: { type: String, required: true },
+    description: { type: String, required: true, maxlength: 820 }, // Description ajoutée
     yearBuilt: { type: Number, required: false },
     pool: { type: Boolean, default: false },
     propertyType: { type: String, required: true },
-    bathrooms: { type: Number, required: false }, // Salles de douche
-    toilets: { type: Number, required: false }, // Toilettes
-    elevator: { type: Boolean, default: false }, // Ascenseur
-    fireplace: { type: Boolean, default: false }, // Cheminée
-    internet: { type: Boolean, default: false }, // Internet
-    doubleGlazing: { type: Boolean, default: false }, // Double vitrage
-    wateringSystem: { type: Boolean, default: false }, // Arrosage
-    barbecue: { type: Boolean, default: false }, // Barbecue
-    carShelter: { type: Boolean, default: false }, // Abri de voiture
-    parking: { type: Boolean, default: false }, // Parking
-    caretakerHouse: { type: Boolean, default: false }, // Maison de gardien
-    electricShutters: { type: Boolean, default: false }, // Stores électriques
-    outdoorLighting: { type: Boolean, default: false }, // Éclairage extérieur
+    bathrooms: { type: Number, required: false },
+    toilets: { type: Number, required: false },
+    elevator: { type: Boolean, default: false },
+    fireplace: { type: Boolean, default: false },
+    internet: { type: Boolean, default: false },
+    doubleGlazing: { type: Boolean, default: false },
+    wateringSystem: { type: Boolean, default: false },
+    barbecue: { type: Boolean, default: false },
+    carShelter: { type: Boolean, default: false },
+    parking: { type: Boolean, default: false },
+    caretakerHouse: { type: Boolean, default: false },
+    electricShutters: { type: Boolean, default: false },
+    outdoorLighting: { type: Boolean, default: false },
     url: { type: String, required: false },
     createdAt: { type: Date, default: Date.now },
     views: { type: Number, default: 0 },
