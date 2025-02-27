a8e68257d680fe6dfb25c66316462b29527fae5b
whades <101385345+whadesb@users.noreply.github.com>
2025-02-27 15:27:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1046,8 +1046,8 @@ async function generateLandingPage(property) {
                     <div class="section-title">Visite guidée</div>
                     ${property.description || 'Aucune description fournie.'}
                 </div>
-
-                <div class="price">Prix: ${property.price.toLocaleString('fr-FR')} €</div>
+console.log(`Prix enregistré pour la landing page: ${property.price} (Type: ${typeof property.price})`);
+                <div class="price">Prix: ${Number(property.price).toLocaleString('fr-FR')} €</div>
             </div>
         </div>
 
