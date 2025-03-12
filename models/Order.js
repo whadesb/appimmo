5577000896e59e9a2698292c25d33084cf8540db
whades <101385345+whadesb@users.noreply.github.com>
2025-03-12 22:01:27 +0100

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -16,13 +16,29 @@ const orderSchema = new mongoose.Schema({
     default: 'pending'
   },
   pageUrl: {
-    type: String, // Ajout de l'URL de la commande
+    type: String,
     required: false
+  },
+  orderNumber: {
+    type: String, // Numéro unique généré
+    unique: true,
+    required: true
   },
   createdAt: {
     type: Date,
     default: Date.now
   }
+});
+
+// Fonction pour générer un numéro de commande unique
+orderSchema.pre('save', async function (next) {
+  if (!this.orderNumber) {
+    const datePart = new Date().toISOString().split('T')[0].replace(/-/g, '');
+    const randomPart = Math.floor(1000 + Math.random() * 9000); // Numéro aléatoire à 4 chiffres
+
+    this.orderNumber = `CMD-${datePart}-${randomPart}`;
+  }
+  next();
 });
 
 module.exports = mongoose.model('Order', orderSchema);
