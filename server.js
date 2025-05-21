cce86b4b7cad9597e26ae4fb0eb66934238aad58
whades <101385345+whadesb@users.noreply.github.com>
2025-05-21 20:12:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -72,18 +72,7 @@ app.use(session({
 }
 }));
 passport.use(User.createStrategy());
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
 
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await User.findById(id);
-    done(null, user);
-  } catch (err) {
-    done(err, null);
-  }
-});
 app.use(passport.initialize());
 app.use(passport.session());
 // Middleware
