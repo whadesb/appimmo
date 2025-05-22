1511fd48acb03d9e3986ce54e56bf5f5543b1924
whades <101385345+whadesb@users.noreply.github.com>
2025-05-22 11:38:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -640,6 +640,15 @@ app.get('/:locale/user', isAuthenticated, async (req, res) => {
   if (!user) {
     return res.redirect(`/${locale}/login`);
   }
+  // 👇 Début du debug
+  const userLandingPages = await Property.find({}); // récupère toutes les annonces
+
+  console.log("Liste brute des userId en base :");
+  userLandingPages.forEach(page => {
+    console.log("➡️", page.userId?.toString());
+  });
+  console.log("Utilisateur connecté :", user._id.toString());
+  // 👆 Fin du debug
 
   const userTranslationsPath = `./locales/${locale}/user.json`;
   let userTranslations = {};
@@ -668,15 +677,7 @@ app.get('/:locale/user', isAuthenticated, async (req, res) => {
     userLandingPages // <--- cette ligne manquait
   });
 });
-  // 👇 Début du debug
-  const userLandingPages = await Property.find({}); // récupère toutes les annonces
 
-  console.log("Liste brute des userId en base :");
-  userLandingPages.forEach(page => {
-    console.log("➡️", page.userId?.toString());
-  });
-  console.log("Utilisateur connecté :", user._id.toString());
-  // 👆 Fin du debug
 
 
 app.get('/:locale/enable-2fa', isAuthenticated, async (req, res) => {
