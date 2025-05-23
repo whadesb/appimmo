8b3f964d1a1e46590ab6db09583ece801556b46f
whades <101385345+whadesb@users.noreply.github.com>
2025-05-23 23:30:01 +0200

    Update Page.js

--- models/Page.js
+++ models/Page.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
 
 const PageSchema = new mongoose.Schema({
-    url: { type: String, required: true },
-    views: { type: Number, default: 0 }
+  url: { type: String, required: true },
+  views: { type: Number, default: 0 },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Page', PageSchema);
