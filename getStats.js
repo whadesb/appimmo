171f450939cd1ca1255929491b4d30b6aa50cddc
whades <101385345+whadesb@users.noreply.github.com>
2025-05-28 23:13:25 +0200

    Update getStats.js

--- getStats.js
+++ getStats.js
@@ -53,20 +53,21 @@ async function getPageStats(pagePath, startDate = '30daysAgo', endDate = 'today'
     };
 
     return {
-      views: parseInt(row.metricValues[0]?.value || '0'),
-      users: parseInt(row.metricValues[1]?.value || '0'),
-      scrolls: parseInt(row.metricValues[2]?.value || '0'),
-      source: row.dimensionValues[8]?.value || 'Non défini',
-      device: row.dimensionValues[4]?.value || 'Inconnu',
-      os: row.dimensionValues[5]?.value || 'Inconnu',
-      browser: row.dimensionValues[6]?.value || 'Inconnu',
-      language: row.dimensionValues[7]?.value || 'Inconnu',
-      geo: {
-        country: row.dimensionValues[1]?.value || 'Inconnu',
-        region: row.dimensionValues[2]?.value || 'Inconnu',
-        city: row.dimensionValues[3]?.value || 'Inconnu'
-      }
-    };
+  views: parseInt(row.metricValues[0]?.value || '0'),
+  users: parseInt(row.metricValues[1]?.value || '0'),
+  scrolls: parseInt(row.metricValues[2]?.value || '0'),
+  channel: row.dimensionValues[8]?.value || 'Non défini', 
+  device: row.dimensionValues[4]?.value || 'Inconnu',
+  os: row.dimensionValues[5]?.value || 'Inconnu',
+  browser: row.dimensionValues[6]?.value || 'Inconnu',
+  language: row.dimensionValues[7]?.value || 'Inconnu',
+  geo: {
+    country: row.dimensionValues[1]?.value || 'Inconnu',
+    region: row.dimensionValues[2]?.value || 'Inconnu',
+    city: row.dimensionValues[3]?.value || 'Inconnu'
+  }
+};
+
 
   } catch (error) {
     console.error('❌ Erreur dans getPageStats :', error.message || error);
