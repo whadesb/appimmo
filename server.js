e1a3e9046f3a00e621844521a648a5ab62b253d9
whades <101385345+whadesb@users.noreply.github.com>
2025-05-28 16:22:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1451,25 +1451,15 @@ async function generateLandingPage(property) {
 
       <!-- Google Tag Manager -->
       <script>
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','${GTM_ID}');
-  </script>
-  <!-- Fin Google Tag Manager -->
-<!-- Leaflet CSS -->
-<link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
-<!-- Leaflet JS -->
-<script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
- <title>${keywords[0]} – ${property.propertyType} à ${property.city}</title>
-<meta name="description" content="${keywords[0]} à ${property.city}. ${property.description?.slice(0, 150) || ''}">
-<meta name="keywords" content="${keywords.join(', ')}">
-<title>${property.propertyType} à vendre à ${city}</title>
-   <meta name="description" content="${property.description?.slice(0, 160)}">
-  <link href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" rel="stylesheet" />
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+      </script>
+      <!-- Fin Google Tag Manager -->
+
+      <script type="application/ld+json">${JSON.stringify(jsonLD)}</script>
 
   <style>
     * {
