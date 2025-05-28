0e83778e4a94ef84edc35095c35712144e7adb94
whades <101385345+whadesb@users.noreply.github.com>
2025-05-28 13:23:08 +0200

    Create seoKeywords.js

---
+++ utils/seoKeywords.js
@@ -0,0 +1,55 @@
+// utils/seoKeywords.js
+
+const seoKeywords = {
+  fr: {
+    France: [
+      "maison à vendre",
+      "appartement à Paris",
+      "immobilier à Marseille",
+      "investissement immobilier France",
+      "résidence secondaire en France",
+      "villa bord de mer"
+    ],
+    Belgique: [
+      "immobilier Bruxelles",
+      "acheter maison Belgique",
+      "appartement Liège",
+      "maison à vendre Belgique"
+    ],
+    Portugal: [
+      "maison Lisbonne",
+      "achat immobilier Algarve",
+      "appartement Porto"
+    ],
+    Espagne: [
+      "immobilier Barcelone",
+      "acheter en Espagne",
+      "villa Costa Brava"
+    ]
+  },
+  en: {
+    France: [
+      "house for sale in France",
+      "real estate Paris",
+      "buy property in France",
+      "French Riviera property"
+    ],
+    Belgium: [
+      "real estate Belgium",
+      "buy house in Brussels",
+      "property in Liège"
+    ],
+    Portugal: [
+      "property in Lisbon",
+      "villa Algarve",
+      "buy apartment Porto"
+    ],
+    Spain: [
+      "house in Barcelona",
+      "Spanish real estate",
+      "buy property in Spain"
+    ]
+  }
+};
+
+module.exports = seoKeywords;
