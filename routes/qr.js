5f95ac88d89c57fec2b3217f84c860e396168d48
whades <101385345+whadesb@users.noreply.github.com>
2025-06-04 23:13:13 +0200

    Fix QR code links to use property URLs

--- routes/qr.js
+++ routes/qr.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const QRCode = require('qrcode');
 const router = express.Router();
+const Property = require('../models/Property');
 
 // Génère le QR code d'une URL donnée
 router.get('/qr/:id', async (req, res) => {
   const propertyId = req.params.id;
-  const fullUrl = `https://uap.immo/landing-pages/${propertyId}.html`;
 
   try {
+    const property = await Property.findById(propertyId);
+    if (!property) {
+      return res.status(404).send('Propriété non trouvée');
+    }
+
+    const fullUrl = `https://uap.immo${property.url}`;
     const qrImage = await QRCode.toDataURL(fullUrl);
-    res.type('html').send(`<img src="${qrImage}" alt="QR Code pour ${fullUrl}" style="width:200px;" />`);
+    res
+      .type('html')
+      .send(`<img src="${qrImage}" alt="QR Code pour ${fullUrl}" style="width:200px;" />`);
   } catch (err) {
     res.status(500).send('Erreur lors de la génération du QR code');
   }
