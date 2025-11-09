63eb58996587f040b49e63018bc75bf973a0819a
whades <101385345+whadesb@users.noreply.github.com>
2025-11-09 23:02:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.use(session({
     }),
     cookie: {
       maxAge: 1000 * 60 * 60 * 24 * 7,
-        secure: true,
+       secure: true,  
         httpOnly: true,
         sameSite: 'None',
     }
